b25d555b670316d23a144d589495fc65e05b11ab
coder <codertonmoy@gmail.com>
2021-09-16 10:42:11 +0600

    I am completing my c course

---
+++ 4.a.AddTwoNumbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+/*****************************************************
+* Author : Tonmoy Kumer Mohonto
+* Purpose : To read two numbers from user and print sum and average of two numbers
+******************************************************/
+void main()
+{
+    int fn, sn, sum;
+    float average;
+
+    //Read values from user
+    printf("Enter first number:");
+    scanf("%d",&fn);
+    printf("Enter second number:");
+    scanf("%d",&sn);
+
+    //Logic - Sum and average
+    sum=fn+sn;
+    average=sum/2.0;
+
+    printf("Sum = %d, Average=%f",sum,average);
+    getch();
+}
